a198a8c63af435b50400163db24f09e9a8c96b21
Laffyftw <laspestrand@gmail.com>
2023-02-16 13:28:08 +0100

    Complete redo of module

---
+++ src/lang.mjs
@@ -0,0 +1,17 @@
+const DEFAULT_LANG = 'en'; // default language code
+let lang = DEFAULT_LANG; // current language code
+
+export function setLang(newLang) {
+  lang = newLang;
+}
+
+export async function t(key) {
+  // Load translation for current language
+  const response = await fetch(`../locales/${lang}.json`);
+  const translations = await response.json();
+
+  console.log(translations);
+
+  // Return translation for key
+  return translations[key];
+}
